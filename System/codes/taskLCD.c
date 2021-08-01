404d8a770b41f938e342faab58ad28b66f2ef079
mateus-caetano <mateus.caetano@alu.ufc.br>
2021-08-01 20:32:37 -0300

    LCD update

--- System/codes/taskLCD.c
+++ System/codes/taskLCD.c
@@ -15,19 +15,19 @@ void taskLCD(void *data)
     xQueuePeek(mb->mb_key, &key, pdMS_TO_TICKS(1));
     xQueuePeek(mb->mb_cpu, &systemUse, pdMS_TO_TICKS(1));
 
-    printf("%s\n", lcd.data);
-    printf("key pressed: %c\n", key.key);
-    printf("led %s\n", led.led_color == verde ? "verde\0" : "vermelho\0");
-    printf("sensor 1 data: %f\n", sensorsData.sensor1);
-    printf("sensor 2 data: %f\n", sensorsData.sensor2);
-    printf("CPU use: %.2f\n", systemUse);
-
+    if (lcd.data != "")
+    {
+        printf("%s\n", lcd.data);
+        printf("key pressed: %c\n", key.key);
+        printf("led %s\n", led.led_color == verde ? "verde\0" : "vermelho\0");
+        printf("sensor 1 data: %f\n", sensorsData.sensor1);
+        printf("sensor 2 data: %f\n", sensorsData.sensor2);
+        printf("CPU use: %.2f\n", systemUse);
+    }
     lcd.data = "";
-    key.key = "";
-    led.led_color = 0;
-    sensorsData.sensor1 = 0;
-    sensorsData.sensor1 = 0;
-    systemUse = 0;
 
     xQueueOverwrite(mb->mb_lcd, &lcd);
+    xQueueOverwrite(mb->mb_led, &led);
+    xQueueOverwrite(mb->mb_key, &key);
+    xQueueOverwrite(mb->mb_sensor, &sensorsData);
 }
