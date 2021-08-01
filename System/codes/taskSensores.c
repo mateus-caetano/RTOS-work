84a5648a77d92d2737cd077d72461a367f8ea57e
franciscothiagoufc <franciscothiago@alu.ufc.br>
2021-08-01 11:18:16 -0300

    Update em tudo

---
+++ System/codes/taskSensores.c
@@ -0,0 +1,9 @@
+#include <System.h>
+#include <FreeRTOS.h>
+#include <queue.h>
+
+void taskSensores(void * data)
+{
+	struct systemData * mb = (struct systemData *) data;
+	//printf("Olá mundo, estou triste\n");
+}
